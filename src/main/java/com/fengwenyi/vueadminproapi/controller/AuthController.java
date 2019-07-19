6dc892ea08fb1656de9e5e1b0445a9490328b3d8
冯文议 <xfsy_2015@163.com>
2019-07-19 18:28:39 +0800

    修改 token 结构

--- src/main/java/com/fengwenyi/vueadminproapi/controller/AuthController.java
+++ src/main/java/com/fengwenyi/vueadminproapi/controller/AuthController.java
@@ -2,16 +2,16 @@ package com.fengwenyi.vueadminproapi.controller;
 
 import com.fengwenyi.vueadminproapi.entity.Login;
 import net.iutil.ApiResult;
+import net.iutil.javalib.util.IdUtils;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 /**
- * 认证
+ * 认证，登录 / 登出
  * @author Erwin Feng
  * @since 2019-06-08 10:21
  */
@@ -21,26 +21,44 @@ import java.util.UUID;
         produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class AuthController {
 
+    /**
+     * 登录示例
+     * @param login
+     * @return
+     */
     @PostMapping("/login")
     public ApiResult login(@RequestBody Login login) {
         String account = login.getAccount();
         String password = login.getPassword();
         if (StringUtils.isEmpty(account))
-            return ApiResult.error().setCode(10001).setMsg("账号不能为空");
+            return ApiResult.error().setMsg("账号不能为空");
         if (StringUtils.isEmpty(password))
-            return ApiResult.error().setCode(10002).setMsg("密码不能为空");
+            return ApiResult.error().setMsg("密码不能为空");
         if (!account.equals("admin"))
-            return ApiResult.error().setCode(10003).setMsg("账号不存在");
+            return ApiResult.error().setMsg("账号不存在");
         if (!password.equals("admin@1234"))
-            return ApiResult.error().setCode(10004).setMsg("密码不正确");
+            return ApiResult.error().setMsg("密码不正确");
+        String uid = IdUtils.getIdByUUID();
+        String token = IdUtils.getIdByUUID();
+        // 可指定有效期
+        // 可自定义策略保存uid、token
+        // write code
         Map<String, String> map = new HashMap<>();
-        map.put("token", UUID.randomUUID().toString());
-        map.put("uid", UUID.randomUUID().toString());
+        map.put("token", uid + "_" + token);
         return ApiResult.success(map);
     }
 
+    /**
+     * 登出示例
+     * @param token
+     * @return
+     */
     @GetMapping("/logout")
     public ApiResult logout(@RequestHeader String token) {
+        // 清空token
+        // write code
+
+        // return
         return ApiResult.success();
     }
 
